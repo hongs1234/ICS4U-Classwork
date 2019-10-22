0f40b5e656db5f771f9ba3f5c9c972e48390cb92
Hong <stella.hong20@ycdsbk12.ca>
2019-10-22 12:50:54 -0400

    began Q2

---
+++ Classes/prep_for_test.py
@@ -0,0 +1,29 @@
+# Q1
+class Food:
+    def __init__(self, name: str, cost: int, nutrition: int):
+        self.name = name
+        self.cost = cost
+        self.nutrition = nutrition
+
+food = Food("Bread", 4, 10)
+
+#Q2
+class Dog:
+    def __init__(self, name: str, breed: str, happiness: int):
+        self.name = name
+        self. breed = breed
+        self.happiness = happiness
+    
+    def eat(self, food):
+        pass
+
+    def bark():
+        print("RUFF RUFF!")
+
+    def __str__():
+        print(f"""Name: {name}
+        Happiness: {happiness}""")
+
+dog = Dog()
+
+def main():
