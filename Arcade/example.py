a84a54f6e95d744a921a9c8a8566ea281997c4d0
Hong <stella.hong20@ycdsbk12.ca>
2019-11-12 13:35:54 -0500

    arcade

---
+++ Arcade/example.py
@@ -0,0 +1,53 @@
+import arcade
+
+
+WIDTH = 640
+HEIGHT = 480
+
+
+class Sprite:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+
+window = arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
+
+
+def setup():
+    arcade.set_background_color(arcade.color.WHITE)
+    arcade.schedule(update, 1/60)
+    arcade.run()
+
+
+def update(delta_time):
+    pass
+
+
+@window.event
+def on_draw():
+    arcade.start_render()
+    # Draw in here...
+    player = Sprite(200, 200)
+
+
+    arcade.draw_circle_filled(player.x, player.y, 25, arcade.color.BLUE)
+
+
+@window.event
+def on_key_press(key, modifiers):
+    pass
+
+
+@window.event
+def on_key_release(key, modifiers):
+    pass
+
+
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    pass
+
+
+if __name__ == '__main__':
+    setup()
