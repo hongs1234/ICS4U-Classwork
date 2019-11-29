dc6d13f3694630db87fafe253c8fa04866a46c8d
Hong <stella.hong20@ycdsbk12.ca>
2019-11-29 14:27:39 -0500

    recursion practice

---
+++ Algorithms/Recursion_Exercise.py
@@ -0,0 +1,17 @@
+# Fibonacci
+def fibonacci(num: int):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    
+    return fibonacci(num-1) + fibonacci(num-2)
+
+# Triangle
+
+
+# sumDigits
+# def sumDigits()
+
+# Allstar
+# def allStar(word: str):
