1abe94ab0e06314f4d41719046dc2d4d9097041a
Hong <stella.hong20@ycdsbk12.ca>
2019-12-09 14:16:06 -0500

    updates

--- Algorithms/Recursion_Exercise.py
+++ Algorithms/Recursion_Exercise.py
@@ -15,3 +15,47 @@ def fibonacci(num: int):
 
 # Allstar
 # def allStar(word: str):
+
+
+# bunnyEars2
+def bunnyEars2(n: int):
+    if n == 0:
+        return 0
+    if n % 2 == 0:
+        return 3 + bunnyEars2(n-1)
+    else:
+        return 2 + bunnyEars2(n-1)
+
+# triangle
+def triangle(n: int):
+    if n == 0:
+        return 0
+    return n + triangle(n-1)
+
+
+# sumDigits
+def sumDigits(n: int):
+    if n == 0:
+        return 0
+    right_most = n % 10
+    n = n/10
+    return right_most + sumDigits(n)
+
+# changePi
+def changePi(word: str):
+    n = len(word) - 1
+    if n == 1:
+        return word[n]
+    if word[n-1] + word[n] == "pi":
+        return "3.14" + changePi(n-1)
+    else:
+        return word[n] + changePi(n-1)
+
+
+# noX
+def noX(word: str):
+    n = 0
+    if n == word[-1]:
+        return word
+    if word[n] == "x":
+        return noX(word[])
