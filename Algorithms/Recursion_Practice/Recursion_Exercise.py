e23a7bba9d4e67b4183eb632add07a70db9a6438
Hong <stella.hong20@ycdsbk12.ca>
2019-12-12 12:58:46 -0500

    recursion practice

--- Algorithms/Recursion_Practice/Recursion_Exercise.py
+++ Algorithms/Recursion_Practice/Recursion_Exercise.py
@@ -1,6 +1,18 @@
 from typing import List
 
-# Fibonacci
+# 01-factorial
+def factorial(n: int):
+    if n == 1:
+        return 1
+    return n * factorial(n-1)
+
+# 02-bunnyEars
+def bunnyEars(n:int):
+    if n == 0:
+        return 0
+    return 2 + bunnyEars(n-1)
+
+# 03-Fibonacci
 def fibonacci(num: int):
     if num == 0:
         return 0
@@ -9,17 +21,7 @@ def fibonacci(num: int):
     
     return fibonacci(num-1) + fibonacci(num-2)
 
-# Triangle
-
-
-# sumDigits
-# def sumDigits()
-
-# Allstar
-# def allStar(word: str):
-
-
-# bunnyEars2
+# 04-bunnyEars2
 def bunnyEars2(n: int):
     if n == 0:
         return 0
@@ -28,21 +30,50 @@ def bunnyEars2(n: int):
     else:
         return 2 + bunnyEars2(n-1)
 
-# triangle
+# 05-triangle
 def triangle(n: int):
     if n == 0:
         return 0
     return n + triangle(n-1)
 
 
-# sumDigits
+# 06-sumDigits
 def sumDigits(n: int):
     if n == 0:
         return 0
     right_most = n % 10
     return right_most + sumDigits(n/10)
 
-# changePi
+# 07-count7
+def count7(n: int):
+    if n == 0:
+        return 0
+    right_most = n % 10
+    if right_most == 7:
+        return 1 + count7(n//10)
+    return count7(n//10)
+
+# 08-count8
+def count8(n: int):
+    if n == 0:
+        return 0
+    right_most = n % 10
+    last_two = n % 100
+    if last_two == 88:
+        return 2 + count8(n//10)
+    elif right_most == 8:
+        return 1 + count8(n//10)
+    return count8(n//10)
+
+# 09-powerN
+
+# 10-countX
+
+# 11-countHi
+
+# 12-changeXY 
+
+# 13-changePi
 def changePi(word: str):
     if len(word) == 0:
         return ""
@@ -50,24 +81,39 @@ def changePi(word: str):
         return "3.14" + changePi(word[2:])
     return word[0] + changePi(word[1:])
 
-
-# noX
+# 14-noX
 def noX(word: str):
-    if len(word) <= 1:
+    if len(word) < 1:
         return ""
     a = word[0]
     if a == "x":
         return noX(word[1:])
     return a + noX(word[1:])
 
-
-# array6
+# 15-array6
 def array6(nums: List[int], n: int):
     if len(nums) == n:
         return 0
     return nums[n] == 6 or array6(nums, n + 1)
 
-# stringClean
+# 16-array11
+
+# 17-array220
+
+# 18-Allstar
+# def allStar(word: str):
+
+# 19-pairStar
+
+# 20-endX
+
+# 21-countPairs
+
+# 22-countABC
+
+# 23-count11
+
+# 24-stringClean
 def stringClean(word: str):
     if len(word) <= 1:
         return word
@@ -77,7 +123,17 @@ def stringClean(word: str):
         return stringClean(word[1:])
     return a + stringClean(word[1:])
 
-# strDist
+# 25-countHi2
+
+# 26-parenBit
+
+# 27-nestParen
+
+# 28-strCount
+
+# 29-strCopies
+
+# 30-strDist
 def strDist(word: str, substring: str):
     if len(word) < len(substring):
         return 0
--- Algorithms/Recursion_Practice/test_Recursion_Exercise.py
+++ Algorithms/Recursion_Practice/test_Recursion_Exercise.py
@@ -1,251 +1,476 @@
 from Recursion_Exercise import *
 
-# fibonacci
-def test_0():
-    assert fibonacci(0) == 0
 
-def test_1():
-    assert fibonacci(1) == 1
+# # 01-factorial
+# def test_0():
+#     assert factorial(1) == 1
 
-def test_2():
-    assert fibonacci(2) == 1
+# def test_1():
+#     assert factorial(2) == 2
 
-def test_3():
-    assert fibonacci(3) == 2
+# def test_2():
+#     assert factorial(3) == 6
 
-def test_4():
-    assert fibonacci(4) == 3
+# def test_3():
+#     assert factorial(4) == 24
 
-def test_5():
-    assert fibonacci(5) == 5
+# def test_4():
+#     assert factorial(5) == 120
 
-def test_6():
-    assert fibonacci(6) == 8
+# def test_5():
+#     assert factorial(6) == 720
 
-def test_7():
-    assert fibonacci(7) == 13
+# def test_6():
+#     assert factorial(7) == 5040
 
+# def test_7():
+#     assert factorial(8) == 40320
 
-# bunnyEars2
-def test_0():
-    assert bunnyEars2(0) == 0
-
-def test_1():
-    assert bunnyEars2(1) == 2
-
-def test_2():
-    assert bunnyEars2(2) == 5
-
-def test_3():
-    assert bunnyEars2(3) == 7
-
-def test_4():
-    assert bunnyEars2(4) == 10
-
-def test_5():
-    assert bunnyEars2(5) == 12
-
-def test_6():
-    assert bunnyEars2(6) == 15
-
-def test_7():
-    assert bunnyEars2(10) == 25
+# def test_8():
+#     assert factorial(12) == 479001600
 
 
-# triangle
-def test_0():
-    assert triangle(0) == 0
+# # 02-bunnyEars
+# def test_0():
+#     assert bunnyEars(0) == 0
 
-def test_1():
-    assert triangle(1) == 1
+# def test_1():
+#     assert bunnyEars(1) == 2
 
-def test_2():
-    assert triangle(2) == 3
+# def test_2():
+#     assert bunnyEars(2) == 4
 
-def test_3():
-    assert triangle(3) == 6
+# def test_3():
+#     assert bunnyEars(3) == 6
 
-def test_4():
-    assert triangle(4) == 10
+# def test_4():
+#     assert bunnyEars(4) == 8
 
-def test_5():
-    assert triangle(5) == 15
+# def test_5():
+#     assert bunnyEars(5) == 10
 
-def test_6():
-    assert triangle(6) == 21
+# def test_6():
+#     assert bunnyEars(12) == 24
 
-def test_7():
-    assert triangle(7) == 28
+# def test_7():
+#     assert bunnyEars(50) == 100
+
+# def test_8():
+#     assert bunnyEars(234) == 468
 
 
-# sumDigits
-def test_0():
-    assert sumDigits(126) == 9
+# # 03-fibonacci
+# def test_0():
+#     assert fibonacci(0) == 0
 
-def test_1():
-    assert sumDigits(49) == 13
+# def test_1():
+#     assert fibonacci(1) == 1
 
-def test_2():
-    assert sumDigits(12) == 3
+# def test_2():
+#     assert fibonacci(2) == 1
 
-def test_3():
-    assert sumDigits(10) == 1
+# def test_3():
+#     assert fibonacci(3) == 2
 
-def test_4():
-    assert sumDigits(1) == 1
+# def test_4():
+#     assert fibonacci(4) == 3
 
-def test_5():
-    assert sumDigits(0) == 0
+# def test_5():
+#     assert fibonacci(5) == 5
 
-def test_6():
-    assert sumDigits(730) == 10
+# def test_6():
+#     assert fibonacci(6) == 8
 
-def test_7():
-    assert sumDigits(1111) == 4
+# def test_7():
+#     assert fibonacci(7) == 13
 
-def test_8():
-    assert sumDigits(11111) == 5
 
-def test_9():
-    assert sumDigits(10110) == 3
+# # 04-bunnyEars2
+# def test_0():
+#     assert bunnyEars2(0) == 0
+
+# def test_1():
+#     assert bunnyEars2(1) == 2
+
+# def test_2():
+#     assert bunnyEars2(2) == 5
+
+# def test_3():
+#     assert bunnyEars2(3) == 7
+
+# def test_4():
+#     assert bunnyEars2(4) == 10
+
+# def test_5():
+#     assert bunnyEars2(5) == 12
+
+# def test_6():
+#     assert bunnyEars2(6) == 15
+
+# def test_7():
+#     assert bunnyEars2(10) == 25
+
+
+# # 05-triangle
+# def test_0():
+#     assert triangle(0) == 0
+
+# def test_1():
+#     assert triangle(1) == 1
+
+# def test_2():
+#     assert triangle(2) == 3
+
+# def test_3():
+#     assert triangle(3) == 6
+
+# def test_4():
+#     assert triangle(4) == 10
+
+# def test_5():
+#     assert triangle(5) == 15
+
+# def test_6():
+#     assert triangle(6) == 21
+
+# def test_7():
+#     assert triangle(7) == 28
+
+
+# # 06-sumDigits
+# def test_0():
+#     assert sumDigits(126) == 9
+
+# def test_1():
+#     assert sumDigits(49) == 13
+
+# def test_2():
+#     assert sumDigits(12) == 3
+
+# def test_3():
+#     assert sumDigits(10) == 1
+
+# def test_4():
+#     assert sumDigits(1) == 1
+
+# def test_5():
+#     assert sumDigits(0) == 0
+
+# def test_6():
+#     assert sumDigits(730) == 10
+
+# def test_7():
+#     assert sumDigits(1111) == 4
+
+# def test_8():
+#     assert sumDigits(11111) == 5
+
+# def test_9():
+#     assert sumDigits(10110) == 3
 
 # def test_10():
 #     assert sumDigits(235) == 10
 
 
-# change Pi
+# # 07-count7
+# def test_0():
+#     assert count7(717) == 2
+
+# def test_1():
+#     assert count7(7) == 1
+
+# def test_2():
+#     assert count7(123) == 0
+
+# def test_3():
+#     assert count7(77) == 2
+
+# def test_4():
+#     assert count7(7123) == 1
+
+# def test_5():
+#     assert count7(771237) == 3
+
+# def test_6():
+#     assert count7(771737) == 4
+
+# def test_7():
+#     assert count7(47571) == 2
+
+# def test_8():
+#     assert count7(777777) == 6
+
+# def test_9():
+#     assert count7(70701277) == 4
+
+# def test_10():
+#     assert count7(777576197) == 5
+
+# def test_11():
+#     assert count7(99999) == 0
+
+# def test_12():
+#     assert count7(99799) == 1
+
+
+# 08-count8
 def test_0():
-    assert changePi("xpix") == "x3.14x"
+    assert count8(8) == 1
 
 def test_1():
-    assert changePi("pipi") == "3.143.14"
+    assert count8(818) == 2
 
 def test_2():
-    assert changePi("pip") == "3.14p"
+    assert count8(8818) == 4
 
 def test_3():
-    assert changePi("pi") == "3.14"
+    assert count8(8088) == 4
 
 def test_4():
-    assert changePi("hip") == "hip"
+    assert count8(123) == 0
 
 def test_5():
-    assert changePi("p") == "p"
+    assert count8(81238) == 2
 
 def test_6():
-    assert changePi("x") == "x"
+    assert count8(88788) == 6
 
 def test_7():
-    assert changePi("") == ""
+    assert count8(8234) == 1
 
 def test_8():
-    assert changePi("pixx") == "3.14xx"
+    assert count8(2348) == 1
 
 def test_9():
-    assert changePi("xyzzy") == "xyzzy"
-
-# array6
-def test_0():
-    assert array6([1, 6, 4], 0) == True
-
-def test_1():
-    assert array6([1, 4], 0) == False
-
-def test_2():
-    assert array6([6], 0) == True
-
-def test_3():
-    assert array6([], 0) == False
-
-def test_4():
-    assert array6([6, 2, 2], 0) == True
-
-def test_5():
-    assert array6([2, 5], 0) == False
-
-def test_6():
-    assert array6([1, 9, 4, 6, 6], 0) == True
-
-def test_7():
-    assert array6([2, 5, 6], 0) == True
-
-# noX
-def test_0():
-    assert noX("xaxb") == "ab"
-
-def test_1():
-    assert noX("abc") == "abc"
-
-def test_2():
-    assert noX("xx") == ""
-
-def test_3():
-    assert noX("") == ""
-
-def test_4():
-    assert noX("axxbxx") == "ab"
-
-def test_5():
-    assert noX("Hellox") == "Hello"
-
-# stringClean
-def test_0():
-    assert stringClean("yyzzza") == "yza"
-
-def test_1():
-    assert stringClean("abbbcdd") == "abcd"
-
-def test_2():
-    assert stringClean("Hello") == "Helo"
-
-def test_3():
-    assert stringClean("XXabcYY") == "XabcY"
-
-def test_4():
-    assert stringClean("112ab445") == "12ab45"
-
-def test_5():
-    assert stringClean("Hello Bookkeeper") == "Helo Bokeper"
-
-# strDist
-def test_0():
-    assert strDist("catcowcat", "cat") == 9
-
-def test_1():
-    assert strDist("catcowcat", "cow") == 3
-
-def test_2():
-    assert strDist("cccatcowcatxx", "cat") == 9
-
-def test_3():
-    assert strDist("abccatcowcatcatxyz", "cat") == 12
-
-def test_4():
-    assert strDist("xyx", "x") == 3
-
-def test_5():
-    assert strDist("xyx", "y") == 1
-
-def test_6():
-    assert strDist("xyx", "z") == 0
-
-def test_7():
-    assert strDist("z", "z") == 1
-
-def test_8():
-    assert strDist("x", "z") == 0
-
-def test_9():
-    assert strDist("", "z") == 0
+    assert count8(23884) == 3
 
 def test_10():
-    assert strDist("hiHellohihihi", "hi") == 13
+    assert count8(0) == 0
 
 def test_11():
-    assert strDist("hiHellohihihi", "hih") == 5
+    assert count8(1818188) == 5
 
 def test_12():
-    assert strDist("hiHellohihihi", "o") == 1
+    assert count8(8818181) == 5
 
 def test_13():
-    assert strDist("hiHellohihihi", "ll") == 2
+    assert count8(1080) == 1
+
+def test_14():
+    assert count8(188) == 3
+
+def test_15():
+    assert count8(88888) == 9
+
+def test_16():
+    assert count8(9898) == 2
+
+def test_17():
+    assert count8(78) == 1
+
+
+# # 09-powerN
+
+# # 10-countX
+
+# # 11-countHi
+
+# # 12-changeXY 
+# def test_0():
+#     assert changeXY("codex") == "codey"
+
+# def test_1():
+#     assert changeXY("xxhixx") == "yyhiyy"
+
+# def test_2():
+#     assert changeXY("xhixhix") == "yhiyhiy"
+
+# def test_3():
+#     assert changeXY("hiy") == "hiy"
+
+# def test_4():
+#     assert changeXY("h") == "h"
+
+# def test_5():
+#     assert changeXY("x") == "y"
+
+# def test_6():
+#     assert changeXY("") == ""
+
+# def test_7():
+#     assert changeXY("xxx") == "yyy"
+
+# def test_8():
+#     assert changeXY("yyhxyi") == "yyhyyi"
+
+# def test_9():
+#     assert changeXY("hihi") == "hihi"
+
+
+# # 13-change Pi
+# def test_0():
+#     assert changePi("xpix") == "x3.14x"
+
+# def test_1():
+#     assert changePi("pipi") == "3.143.14"
+
+# def test_2():
+#     assert changePi("pip") == "3.14p"
+
+# def test_3():
+#     assert changePi("pi") == "3.14"
+
+# def test_4():
+#     assert changePi("hip") == "hip"
+
+# def test_5():
+#     assert changePi("p") == "p"
+
+# def test_6():
+#     assert changePi("x") == "x"
+
+# def test_7():
+#     assert changePi("") == ""
+
+# def test_8():
+#     assert changePi("pixx") == "3.14xx"
+
+# def test_9():
+#     assert changePi("xyzzy") == "xyzzy"
+
+
+# # 14-noX
+# def test_0():
+#     assert noX("xaxb") == "ab"
+
+# def test_1():
+#     assert noX("abc") == "abc"
+
+# def test_2():
+#     assert noX("xx") == ""
+
+# def test_3():
+#     assert noX("") == ""
+
+# def test_4():
+#     assert noX("axxbxx") == "ab"
+
+# def test_5():
+#     assert noX("Hellox") == "Hello"
+
+
+# # 15-array6
+# def test_0():
+#     assert array6([1, 6, 4], 0) == True
+
+# def test_1():
+#     assert array6([1, 4], 0) == False
+
+# def test_2():
+#     assert array6([6], 0) == True
+
+# def test_3():
+#     assert array6([], 0) == False
+
+# def test_4():
+#     assert array6([6, 2, 2], 0) == True
+
+# def test_5():
+#     assert array6([2, 5], 0) == False
+
+# def test_6():
+#     assert array6([1, 9, 4, 6, 6], 0) == True
+
+# def test_7():
+#     assert array6([2, 5, 6], 0) == True
+
+
+# # 16-array11
+
+# # 17-array220
+
+# # 18-Allstar
+
+# # 19-pairStar
+
+# # 20-endX
+
+# # 21-countPairs
+
+# # 22-countABC
+
+# # 23-count11
+
+# # 24-stringClean
+# def test_0():
+#     assert stringClean("yyzzza") == "yza"
+
+# def test_1():
+#     assert stringClean("abbbcdd") == "abcd"
+
+# def test_2():
+#     assert stringClean("Hello") == "Helo"
+
+# def test_3():
+#     assert stringClean("XXabcYY") == "XabcY"
+
+# def test_4():
+#     assert stringClean("112ab445") == "12ab45"
+
+# def test_5():
+#     assert stringClean("Hello Bookkeeper") == "Helo Bokeper"
+
+
+# # 25-countHi2
+
+# # 26-parenBit
+
+# # 27-nestParen
+
+# # 28-strCount
+
+# # 29-strCopies
+
+
+# # 30-strDist
+# def test_0():
+#     assert strDist("catcowcat", "cat") == 9
+
+# def test_1():
+#     assert strDist("catcowcat", "cow") == 3
+
+# def test_2():
+#     assert strDist("cccatcowcatxx", "cat") == 9
+
+# def test_3():
+#     assert strDist("abccatcowcatcatxyz", "cat") == 12
+
+# def test_4():
+#     assert strDist("xyx", "x") == 3
+
+# def test_5():
+#     assert strDist("xyx", "y") == 1
+
+# def test_6():
+#     assert strDist("xyx", "z") == 0
+
+# def test_7():
+#     assert strDist("z", "z") == 1
+
+# def test_8():
+#     assert strDist("x", "z") == 0
+
+# def test_9():
+#     assert strDist("", "z") == 0
+
+# def test_10():
+#     assert strDist("hiHellohihihi", "hi") == 13
+
+# def test_11():
+#     assert strDist("hiHellohihihi", "hih") == 5
+
+# def test_12():
+#     assert strDist("hiHellohihihi", "o") == 1
+
+# def test_13():
+#     assert strDist("hiHellohihihi", "ll") == 2
